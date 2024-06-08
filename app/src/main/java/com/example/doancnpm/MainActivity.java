c27d6ef5c126ce292a99d5827b651dc5c5949486
nghieee <145263187+nghieee@users.noreply.github.com>
2024-06-08 11:09:46 +0700

    update

--- app/src/main/java/com/example/doancnpm/MainActivity.java
+++ app/src/main/java/com/example/doancnpm/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.doancnpm;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tV_sign_up = findViewById(R.id.tV_sign_up);
+
+        SpannableString spannableString = new SpannableString("Don't have an Account? SIGN UP");
+        //Thiết lập màu chữ
+        ForegroundColorSpan colorSpan = new ForegroundColorSpan(Color.BLUE);
+        spannableString.setSpan(colorSpan, 23, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        //Thiết lập gạch chân
+        UnderlineSpan underlineSpan = new UnderlineSpan();
+        spannableString.setSpan(underlineSpan, 23, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        //Thiết lập sự kiện click
+        ClickableSpan click_sign_up = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Toast.makeText(getApplicationContext(), "Sign Up clicked", Toast.LENGTH_SHORT).show();
+            }
+        };
+        spannableString.setSpan(click_sign_up, 23, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        tV_sign_up.setMovementMethod(LinkMovementMethod.getInstance());
+        tV_sign_up.setText(spannableString);
+
     }
 }
